deb77f4deae816fcfb2a1d1ff0d2349bfd4d2432
i200760@nu.edu.pk <95473627+Khadijatulkubra001@users.noreply.github.com>
2023-06-07 02:38:07 +0500

    commit frontend

---
+++ src/Service/Api.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const usersUrl = 'https://projects-backend-tjjy.onrender.com';
+
+export const getProject = async (id) => {
+    id = id || '';
+    return await axios.get(`${usersUrl}/${id}`);
+}
+
+export const addProject = async (user) => {
+    return await axios.post(`${usersUrl}/add`, user);
+}
+
+export const deleteProject = async (id) => {
+    return await axios.delete(`${usersUrl}/${id}`);
+}
+
+export const updateProject = async (id, user) => {
+    return await axios.put(`${usersUrl}/${id}`, user)
+}
